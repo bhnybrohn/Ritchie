5f48bcc61812fb3c944a93c05bad0b9312ffa5b6
westernwells <rajiorazaq@gmail.com>
2020-05-18 03:40:46 +0100

    now using template engine

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ Thats all the files i could push
 thanks in advance bro
 and again the axios was not working so i wanted to parse the html to nodejs but not working 
 but you go see everything, node nah server hn dey nah why 
+
+Try read on template engine it is used while doing server side rendering
+i used ejs but are lot of other ones out there.
+
+You will noticed that i have created a folder called view => pages, that is where you template filles lives in.
+
+try use a DB for saving the reigistration details after saving successfully render the success page.
+
+when building your admin page the template engine will be very handy.
+
+all the best Bossman
+
+Raji.
--- app.js
+++ app.js
@@ -1,48 +1,69 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const postRoutes = require('./routes/admin');
-//const script = require('./public/script')
-
-
-
 const app = express()
-const stackPost = [];
+// const postRoutes = require('./routes/admin');
 //app.use('/', postRoutes);
+// const stackPost = [];
+
+// setting template engine
+app.set('view engine', 'ejs');
+
+
+// configuring the body parser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}));
 
+// public folder for static asserts e,g css and images
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.post('/', async(req,res, next)=>{
+// Post route for the form submission
+app.post('/', (req,res)=>{
 
   // const error =()=>{
   //    script.error;
   // }
+  // parsing the form data from the req.body
+    const fullname = req.body.fullname
+    const mail = req.body.mail
 
- const post = {
-     Fullname: req.body.Fullname,
-    Mail : req.body.Mail
-  }
-  try{
-    if(Fullname ==='' || Mail ===''){
-
+    // you can do you form validation here if invalid trybthrow error
+    if(fullname ==='' || mail ===''){
+      // throw Error;
     }
-  const jsonParams = JSON.stringify(post)
-  const sentPosts = await jsonParams;
-  res.send(sentPosts)
-  console.log(sentPosts)
-  }
-  catch(err){
-     res.send({message:err})
-  }
 
+    // passing the form data into a post obj
+    const post = { fullname,mail}
+    
+    // trying to pasre strings to JSON
+    const jsonParams = JSON.stringify(post)
+    
+  // after finishing your manipulation with the user data you can render the admin page as you requested or any 
+  // page of your liken
+  res.render("pages/admin",{...post})
+  console.log(jsonParams)
 })
-app.use('/',(req,res,next)=>{
-  res.redirect('/success.html')
-})
+// it renders the index page on /index route
+app.use('/index',(req,res)=>{
+  res.render('pages/index')
+});
 
+// it renders the admin page on /admin route
+app.use('/admin',(req,res)=>{
+  res.render('pages/admin')
 
+});
+// it renders the success page on /success route
+app.use('/success',(req,res)=>{
+  res.render('pages/success')
+
+});
+
+// it renders the 404 page on /404 route
+app.use('/404-page',(req,res)=>{
+  res.render('pages/404-page')
+
+});
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT , console.log(`server started at ${PORT}`) )
